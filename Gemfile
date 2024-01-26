18d44c3c698f8ac2c432dc617fe1e0b4f1c2e649
Christophe JUNIER <christophejunier@gmail.com>
2024-01-26 16:54:51 +0100

    setting up whenever

--- Gemfile
+++ Gemfile
@@ -45,6 +45,8 @@ gem "bootsnap", require: false
 # gem "image_processing", "~> 1.2"
 
 gem "ruby-openai"
+gem 'sidekiq'
+gem 'whenever'
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
--- app/jobs/update_texts_word_job.rb
+++ app/jobs/update_texts_word_job.rb
@@ -1,18 +1,22 @@
 class UpdateTextsWordJob < ActiveJob::Base
-  client = OpenAI::Client.new
+  queue_as :default
 
-  Text.not_recently_updated.find_each do |text|
-    random_word = text.words.sample
-    response = client.chat(
-      parameters: {
-        model: "gpt-3.5-turbo", # Required.
-        messages: [
-          { role: "system", content: "change ce mot" },
-          { role: "user", content: random_word.content }
-        ], # Required.
-      })
-    random_word.update(content: response.dig("choices", 0, "message", "content"))
-    text.update(updated_at: DateTime.current)
-    ActionCable.server.broadcast("the_text", random_word.id)
+  def perform
+    client = OpenAI::Client.new
+
+    Text.not_recently_updated.find_each do |text|
+      random_word = text.words.sample
+      response = client.chat(
+        parameters: {
+          model: "gpt-3.5-turbo", # Required.
+          messages: [
+            { role: "system", content: "change ce mot" },
+            { role: "user", content: random_word.content }
+          ], # Required.
+        })
+      random_word.update(content: response.dig("choices", 0, "message", "content"))
+      text.update(updated_at: DateTime.current)
+      ActionCable.server.broadcast("the_text", random_word.id)
+    end
   end
 end
